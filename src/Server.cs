42e124018c87a9206a43182093c89cf107038436
papaDhk <papa@datahawk.co>
2024-08-07 12:04:30 +0000

    feat: read and parse requests wip

--- src/Server.cs
+++ src/Server.cs
@@ -5,7 +5,7 @@ using System.Text;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
 
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
+var server = new TcpListener(IPAddress.Any, 4221);
 
 try
 {
@@ -21,7 +21,19 @@ try
 
         Console.WriteLine($"Connection established with a client");
 
-        var encodedResponse = Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n");
+        var readBuffer = new byte[1024];
+        
+        var receivedBufferSize = socket.Receive(readBuffer, SocketFlags.None);
+
+        var receivedMessage = Encoding.UTF8.GetString(readBuffer, 0, receivedBufferSize);
+        
+        Console.WriteLine($"Received request: {receivedMessage}");
+
+        var requestTarget = ParseRequestTarget(receivedMessage);
+
+        var responseMessage = requestTarget == "/" ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 NotFound\r\n\r\n";
+        
+        var encodedResponse = Encoding.UTF8.GetBytes(responseMessage);
 
         var result = socket.Send(encodedResponse);
 
@@ -37,4 +49,16 @@ catch (Exception e)
 finally
 {
     server.Stop();
+}
+
+string? ParseRequestTarget(string s)
+{
+    var endOfRequestLineIndex = s.IndexOf("\r\n", StringComparison.Ordinal);
+
+    if (endOfRequestLineIndex == -1) return null;
+
+    var requestLine = s[..endOfRequestLineIndex];
+
+    var requestTarget1 = requestLine.Split(' ')[1];
+    return requestTarget1;
 }
