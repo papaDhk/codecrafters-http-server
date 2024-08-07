133d9c87d471b9feaab24422adb79ef29ce4b97b
papaDhk <papa@datahawk.co>
2024-08-07 15:58:46 +0000

    feat: parse headers into a dictionary

---
+++ src/Constants.cs
@@ -0,0 +1,8 @@
+namespace pmn_http_server;
+
+public class Constants
+{
+    public const string ClrfSeparator = "\r\n";
+    public const string UserAgentHeaderName = "User-Agent";
+
+}
--- src/IncomingHttpRequest.cs
+++ src/IncomingHttpRequest.cs
@@ -7,4 +7,35 @@ public class IncomingHttpRequest
     public string Target { get; set; }
     public string HttpVersion { get; set; }
     public string Body { get; set; }
+    
+    public static IncomingHttpRequest Parse(string requestString)
+    {
+        var requestParts = requestString.Split(Constants.ClrfSeparator);
+        
+        var requestLine = requestParts.FirstOrDefault();
+        //var requestBody = requestParts.LastOrDefault();
+
+        var requestLineParts = requestLine?.Split(' ');
+        
+        var incomingHttpRequest = new IncomingHttpRequest
+        {
+            HttpMethod = requestLineParts?[0],
+            Target = requestLineParts?[1],
+            HttpVersion = requestLineParts?[2],
+        };
+        
+        var beginningOfHeaders = requestString.IndexOf(Constants.ClrfSeparator, StringComparison.Ordinal) + Constants.ClrfSeparator.Length;
+        var endOfHeaders = requestString.IndexOf(Constants.ClrfSeparator+Constants.ClrfSeparator, StringComparison.Ordinal);
+        var headers = requestString[beginningOfHeaders..endOfHeaders].Split(Constants.ClrfSeparator);
+
+        foreach (var header in headers)
+        {
+            var headerNameEndPosition = header.IndexOf(':');
+            var headerName = header[..headerNameEndPosition];
+            var headerValue = header[(headerNameEndPosition + 1)..];
+            incomingHttpRequest.Headers[headerName] = headerValue.Trim();
+        }
+        
+        return incomingHttpRequest;
+    }
 }
